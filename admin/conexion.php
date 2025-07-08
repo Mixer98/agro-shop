9ee914b38d56be80b56d9ebf9c3c3403b55caa1d
mixer98 <risaacdavid@gmail.com>
2025-07-07 20:06:49 -0500

    conexion mobida a un archivo independiente de funciones.php

---
+++ admin/conexion.php
@@ -0,0 +1,13 @@
+
+<?php
+$host = "localhost";
+$usuario = "root";
+$clave = "";
+$bd = "agroshop";
+
+$conexion = mysqli_connect($host, $usuario, $clave, $bd);
+
+if (!$conexion) {
+    die("Error de conexión: " . mysqli_connect_error());
+}
+?>
